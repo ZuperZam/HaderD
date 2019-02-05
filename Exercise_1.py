d5d984d2b7ac1aafe78812156337d79e2dbbb14b
Søren Mathiasen <zuperzam@email.dk>
2019-02-05 17:01:14 +0100

    Hejsa, jeg haderD

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 from __future__ import division, print_function, unicode_literals
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.cm as cm
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 import os
+
+#--------------------------------------------#
+#////////////////////////////////////////////#
+#//////////////////Journal01/////////////////#
+#////////////////////////////////////////////#
+#--------------------------------------------#
+
+#////////////////////////////////////////////#
+#/////////////////Lesson01Qa/////////////////#
+#////////////////////////////////////////////#
+
+print("#########################\nEXERCISE LESSON01 Qa\n#########################")
+for i in range(2):
+    print()
 
 datapath = os.path.join("datasets", "lifesat", "")
 
@@ -55,7 +63,6 @@ for country, pos_text in position_text.items():
 #save_fig('money_happy_scatterplot')
 plt.show()
 
-from sklearn import linear_model
 lin1 = linear_model.LinearRegression()
 Xsample = np.c_[sample_data["GDP per capita"]]
 ysample = np.c_[sample_data["Life satisfaction"]]
@@ -81,3 +88,74 @@ print(theta1)
 
 R2 = lin1.score(Xsample, ysample)
 print(R2)
+
+#////////////////////////////////////////////#
+#/////////////////Lesson02Qa/////////////////#
+#////////////////////////////////////////////#
+
+for i in range(50):
+    if i == 48:
+        print("#########################\nEXERCISE LESSON02 Qa\n#########################")
+    print()
+
+X = np.array([[1,2,3], [4,2,1], [3,8,5], [-9,-1,0]])
+print(X)
+
+#////////////////////////////////////////////#
+#/////////////////Lesson02Qb/////////////////#
+#////////////////////////////////////////////#
+
+for i in range(4):
+    if i == 2:
+        print("#########################\nEXERCISE LESSON02 Qb\n#########################")
+    print()
+
+def L1(matrix):
+    temp = 0
+    temp2 = 0
+    for i in range(len(matrix)):
+        for x in range(len(matrix[0])):
+            temp2 = matrix[i][x]
+            if temp2 < 0:
+                temp += temp2*-1
+            else:
+                temp += temp2
+    return temp
+
+def L2(matrix):
+    temp = 0
+    for i in range(len(matrix)):
+        for x in range(len(matrix[0])):
+            temp += (matrix[i][x])*(matrix[i][x])
+    return np.sqrt(temp)
+
+def L2Dot(vector1, vector2):
+    temp = np.dot(vector1[0], vector2[0])
+    return temp
+        
+
+tx=np.array([[1, 2, 3, -1]])
+ty=np.array([[3,-1, 4,  1]])
+
+expected_d1=8.0
+expected_d2=4.242640687119285
+
+d1=L1(tx-ty)
+d2=L2(tx-ty)
+
+print("L1 =", d1)
+print("L2 =", d2)
+
+print("tx-ty=",tx-ty,", d1-expected_d1=",d1-expected_d1,", d2-expected_d1=",d2-expected_d2)
+
+for i in range(2):
+    print()
+    
+print("Test with linalg.norm =", np.linalg.norm(tx-ty))
+
+for i in range(2):
+    print()
+    
+print(np.dot(tx[0],ty[0]))
+
+print(L2Dot(tx,ty))
