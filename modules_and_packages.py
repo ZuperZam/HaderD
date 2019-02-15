5d3686e40b6a07738f4113d00f6170d2d0022166
Alexander Davidsen <35957336+AlexBigD@users.noreply.github.com>
2019-02-15 13:04:32 +0100

    kdnsdnksdmsan

---
+++ modules_and_packages.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 12:49:54 2019
+
+@author: Alexa
+"""
+from libitmal import __init__
+from libitmal import utils as itmalutils
+from moduletest1 import moduletest1 as test
+
+itmalutils.TestAll()
+
+import sys,os
+sys.path.append(os.path.expanduser('~/itmal'))
+
+print(dir(itmalutils))
+print(itmalutils.__file__)
+
+test.doesItWork()
+
+# Spyder can be forced to sync up to the new modules by going to FILE > Restart (ALT + SHIFT + R)
+# Alternatively, one can also use a reload function that accepts the module to be reloaded as a parameter
+
+class MyClass:
+    myvar = "blah"
+    
+    def __init__(self):
+        
+    def myfun(self):
+        print("This is a message inside the class.")
+        self.myvar = "bleh"
+        return self.myvar
+
+myobjectx = MyClass()
+
+print(myobjectx.myfun())
+print(myobjectx.myvar)
+
+# The meaning of "self" in Python is used to reference instance attributes. Meaning it is used as its own object to
+# reference the the object of a class. This means whenever a variable is changed, for example by doing "self.myvar",
+# the "self" will reference the class object, thus making sure that only the variable inside that class changes.
+
+# If self is not included in a function parameter, the program doesn't know what instance it has to assign any
+# attribute changes to.
+
+# Constructors in Python are declared by first creating an object using a __new__-function. To initialize the newly
+# created object one uses the __init__-function to do so. This function only needs to have self as a parameter.
+# One can provide additional parameters if one needs to set default values for variables. The __new__ and __init__-functions
+# together form what is essentially a constructor in Python.
+
+# Destructors are not required since Python has garbage collection
+
---
+++ moduletest1/moduletest1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 13:17:19 2019
+
+@author: Alexa
+"""
+
+def doesItWork():
+    print("##########\nYes it does!##########")
