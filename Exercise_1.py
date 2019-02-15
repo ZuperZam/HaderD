fd59650c0658959a87af83994e7641736d0f87df
Alexander Davidsen <35957336+AlexBigD@users.noreply.github.com>
2019-02-15 13:23:10 +0100

    lul

--- Exercise_1.py
+++ Exercise_1.py
@@ -99,6 +99,7 @@ for i in range(50):
     print()
 
 X = np.array([[1,2,3], [4,2,1], [3,8,5], [-9,-1,0]])
+y = np.array([1,2,3,4])
 print(X)
 
 #////////////////////////////////////////////#
@@ -117,21 +118,19 @@ def L1(matrix):
         for x in range(len(matrix[0])):
             temp2 = matrix[i][x]
             if temp2 < 0:
-                temp += temp2*-1
-            else:
-                temp += temp2
+                temp2 *= -1
+            temp += temp2
     return temp
 
 def L2(matrix):
     temp = 0
     for i in range(len(matrix)):
         for x in range(len(matrix[0])):
-            temp += (matrix[i][x])*(matrix[i][x])
+            temp += (matrix[i][x])**2
     return np.sqrt(temp)
 
-def L2Dot(vector1, vector2):
-    temp = np.dot(vector1[0], vector2[0])
-    return temp
+def L2Dot(matrix):
+    return np.sqrt(np.dot(matrix[0].T, matrix[0]))
         
 
 tx=np.array([[1, 2, 3, -1]])
@@ -148,6 +147,11 @@ print("L2 =", d2)
 
 print("tx-ty=",tx-ty,", d1-expected_d1=",d1-expected_d1,", d2-expected_d1=",d2-expected_d2)
 
+eps=1E-9
+assert np.fabs(d1-expected_d1)<eps, "L1 dist seems to be wrong" 
+assert np.fabs(d2-expected_d2)<eps, "L2 dist seems to be wrong" 
+
+
 for i in range(2):
     print()
     
@@ -155,7 +159,60 @@ print("Test with linalg.norm =", np.linalg.norm(tx-ty))
 
 for i in range(2):
     print()
-    
-print(np.dot(tx[0],ty[0]))
 
-print(L2Dot(tx,ty))
+print("L2Dot =",L2Dot(tx-ty))
+d2dot=L2Dot(tx-ty)
+print("d2dot-expected_d2=",d2dot-expected_d2)
+assert np.fabs(d2dot-expected_d2)<eps, "L2Ddot dist seem to be wrong" 
+
+#////////////////////////////////////////////#
+#/////////////////Lesson02Qc/////////////////#
+#////////////////////////////////////////////#
+
+for i in range(4):
+    if i == 2:
+        print("#########################\nEXERCISE LESSON02 Qc\n#########################")
+    print()
+
+def h(X):    
+    if X.ndim!=2:
+        raise ValueError("excpeted X to be of ndim=2, got ndim=",X.ndim)
+    if X.shape[0]==0 or X.shape[1]==0:
+        raise ValueError("X got zero data along the 0/1 axis, cannot continue")
+    return X[:,0]
+
+def RMSE(x, y):
+    temp = 0
+    for i in range(x.size):
+        temp += (x[i] - y[i])**2
+    return np.sqrt((1/x.size)*temp)
+
+eps=1E-9
+r=RMSE(h(X),y)
+expected=6.57647321898295
+print("RMSE=",r,", diff=",r-expected)
+assert r-expected<eps, "your RMSE dist seems to be wrong" 
+
+#////////////////////////////////////////////#
+#/////////////////Lesson02Qd/////////////////#
+#////////////////////////////////////////////#
+
+for i in range(4):
+    if i == 2:
+        print("#########################\nEXERCISE LESSON02 Qd\n#########################")
+    print()
+
+def MAE(x, y):
+    temp = 0
+    temp2 = 0
+    for i in range(x.size):
+        temp2 = x[i] + y[i]
+        if temp2 < 0:
+            temp2 *= -1
+        temp += temp2
+    return (1/x.size)*temp
+
+r=MAE(h(X), y)
+expected=3.75
+print("MAE=",r,", diff=",r-expected)
+assert r-expected<eps, "MAE dist seems to be wrong" 
