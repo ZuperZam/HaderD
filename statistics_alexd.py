b05e01436398572947053918b943da8356cb01ac
Alexander Davidsen <35957336+AlexBigD@users.noreply.github.com>
2019-02-19 13:23:27 +0100

    Wee

--- statistics_alexd.py
+++ statistics_alexd.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb  8 12:08:26 2019
-
 @author: Alexa
 """
 
 #statistics.ipynb
 
+#////////////////////////////////////////////#
+#/////////////////Statistics Qa//////////////#
+#////////////////////////////////////////////#
+
+import statistics
+import numpy as np
+
 def MeanAndVariance(y):
+    mean = statistics.mean(y)
+    variance = statistics.variance(y)
     #Caculate mean
-    
-    return make_pair<float,float>(mean, variance)
+    return (mean, variance)
 
 y = np.array([1,2,3,4])
 
@@ -23,3 +30,29 @@ expected_v_unbiased = 1.6666666666666667 # factor 1/(n-1)
 print("m=",m,", diff=", m-expected_m)
 print("v=",v,", diff=", v-expected_v_biased)
 print(v,np.mean(y), np.var(y)) # np.var is biased(n)
+
+
+#////////////////////////////////////////////#
+#/////////////////Statistics Qb//////////////#
+#////////////////////////////////////////////#
+
+import numpy as np
+
+Xi = np.array([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5])
+N = np.size(Xi)
+k = 5
+Xs = np.average(Xi)
+
+def AutoCovarianceMatrix(Xi, N, k, Xs):
+    autoCov = 0
+    for i in np.arange(0, N - k):
+        autoCov += ((Xi[i+k])-Xs)*(Xi[i]-Xs)
+    return (1/(N-1))*autoCov
+
+print("Autovariance:", AutoCovarianceMatrix(Xi, N, k, Xs))
+
+
+#////////////////////////////////////////////#
+#/////////////////Statistics Qc//////////////#
+#////////////////////////////////////////////#
+
