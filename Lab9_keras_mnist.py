83935833c43aba74bcc9c1d3b26e3db7b1417e34
Bechy7 <au568435@uni.au.dk>
2019-05-21 11:12:50 +0200

    yeye

---
+++ Lab9_keras_mnist.py
@@ -0,0 +1,167 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam, SGD
+from keras.utils.np_utils import to_categorical
+
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+
+import numpy as np
+from time import time
+
+np.random.seed(42)
+
+# Build Keras model 
+model = Sequential()
+model.add(Dense(input_dim=784, units=8, activation="tanh", kernel_initializer="normal"))
+model.add(Dense(units=10, activation="softmax"))
+
+#optimizer = SGD(lr=0.1)
+optimizer = Adam(lr=0.1)
+model.compile(loss='categorical_crossentropy', 
+              optimizer=optimizer, 
+              metrics=['categorical_accuracy', 'mean_squared_error', 'mean_absolute_error'])
+
+# Make data
+#X, y = datasets.make_moons(2000, noise=0.20, random_state=42)
+from shutil import copyfileobj
+from six.moves import urllib
+from sklearn.datasets.base import get_data_home
+from sklearn.model_selection import train_test_split
+import os
+
+def fetch_mnist(data_home=None):
+    mnist_alternative_url = "https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat"
+    data_home = get_data_home(data_home=data_home)
+    data_home = os.path.join(data_home, 'mldata')
+    if not os.path.exists(data_home):
+        os.makedirs(data_home)
+    mnist_save_path = os.path.join(data_home, "mnist-original.mat")
+    if not os.path.exists(mnist_save_path):
+        mnist_url = urllib.request.urlopen(mnist_alternative_url)
+        with open(mnist_save_path, "wb") as matlab_file:
+            copyfileobj(mnist_url, matlab_file)
+from sklearn.datasets import fetch_mldata
+
+def MNIST_GetDataSet():
+    fetch_mnist()
+    mnist = fetch_mldata('MNIST original')
+    return(mnist["data"], mnist["target"])
+
+
+X, y = MNIST_GetDataSet()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+y_train_binary = to_categorical(y_train)
+y_test_binary  = to_categorical(y_test)
+
+assert y.ndim==1
+assert y_train_binary.ndim==2
+assert y_test_binary.ndim ==2
+
+# Train
+VERBOSE     = 0
+EPOCHS      = 3
+
+start = time()
+history = model.fit(X_train, y_train_binary, validation_data=(X_test, y_test_binary), epochs=EPOCHS, verbose=VERBOSE)
+t = time()-start
+
+
+
+
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#print(history.history)
+score = model.evaluate(X_test, y_test_binary, verbose=0)
+
+print(f"Training time: {t:0.1f} sec")
+print(f"Test loss:     {score[0]}") # loss is score 0 by definition?
+print(f"Test accuracy: {score[1]}")
+print(f"All scores in history: {score}")
+
+N=4
+FX=60
+FY=4
+A=0.4
+S=4
+
+# Plot org data
+plt.figure(figsize=(FX, FY))
+ax = plt.subplot(1, N, 1)
+colors = ['steelblue' if label == 1 else 'darkred' for label in y]
+plt.scatter(X[:,0], X[:,1], color=colors, alpha=.5)
+plt.show()
+
+# Plot loss
+plt.figure(figsize=(FX, FY))
+ax = plt.subplot(1, N, 2)
+plt.plot(history.history["loss"]    , "b--x", markerfacecolor=(0, 0, 1, A), markersize=S)
+plt.plot(history.history["val_loss"], "g-s" , markerfacecolor=(0, 1, 0, A), markersize=S)
+plt.legend(loc="best", labels=("loss(train)","loss(val)"))
+plt.xlabel("epoch")
+plt.ylabel("loss")
+plt.title("Loss-vs-epoch plot")
+plt.show()
+
+# Plot all metrics + loss
+plt.figure(figsize=(FX, FY))
+ax = plt.subplot(1, N, 3)
+plt.plot(history.history["mean_squared_error"],      "r:x", markerfacecolor=(1, 0, 0, A), markersize=S)
+plt.plot(history.history["val_mean_squared_error"],  "r-x", markerfacecolor=(1, 0, 0, A), markersize=S)
+plt.plot(history.history["mean_absolute_error"],     "b:o", markerfacecolor=(0, 0, 1, A), markersize=S)
+plt.plot(history.history["val_mean_absolute_error"], "b-o", markerfacecolor=(0, 0, 1, A), markersize=S)
+plt.xlabel("epoch")
+plt.ylabel("error")
+plt.xlim((0, EPOCHS))
+plt.legend(loc="best", labels=("mean_squared_error(train)",  "mean_squared_error(val)", 
+                               "mean_absolute_error(train)", "mean_absolute_error(val)", 
+                               "loss(categorical_crossentropy,train)", "loss(categorical_crossentropy,val)"))
+plt.title("Error-vs-epoch plot")
+plt.show()
+
+# Plot accuracy
+plt.figure(figsize=(FX, FY))
+ax = plt.subplot(1, N, 4)
+plt.plot(history.history["categorical_accuracy"],     "m-x", markerfacecolor=(1, 0, 1, A), markersize=S)
+plt.plot(history.history["val_categorical_accuracy"], "m:x", markerfacecolor=(1, 0, 1, A), markersize=S)
+ax.set_ylim([0,1])
+plt.xlabel("epoch")
+plt.ylabel("accuracy")
+plt.xlim((0, EPOCHS))
+plt.legend(loc="lower right", labels=("categorical_accuracy",))
+plt.title("Accuracy-vs-epoch plot")
+plt.show()
+
+
+
+
+
+
+
+
+# Helper function to plot a decision boundary.
+#def plot_decision_boundary(pred_func):
+#    # Set min and max values and give it some padding
+#    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+#    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+#    h = 0.01
+#    # Generate a grid of points with distance h between them
+#    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+#    # Predict the function value for the whole gid
+#    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+#    Z = Z.reshape(xx.shape)
+#    # Plot the contour and training examples
+#    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral,alpha=.8)
+#    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral, alpha=.5)
+#
+## Predict and plot decision boundary
+#plot_decision_boundary(lambda x: model.predict_classes(x, batch_size=200))
+#plt.title("Decision Boundary")
+#plt.show()
