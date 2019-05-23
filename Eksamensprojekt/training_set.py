7acd36de4340ee586736f69138f6dc5b3553c3f5
Søren Mathiasen <zuperzam@email.dk>
2019-05-23 21:20:58 +0200

    er
    push

--- Eksamensprojekt/training_set.py
+++ Eksamensprojekt/training_set.py
@@ -18,16 +18,19 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
+import warnings
+warnings.filterwarnings("ignore")
 
 dataset = []
 dataset_guess = []
 y = []
 y_guess = []
-image_size = 250
+image_size = 500
 
 POSITIVE_IMAGES_PATH = './shrek'
 NEGATIVE_IMAGES_PATH = './not_shrek_v2'
-POSITIVE_GUESS_IMAGES_PATH = './guess_positive'
+POSITIVE_GUESS_IMAGES_PATH = './guess_positive_test'
+NEGATIVE_GUESS_IMAGES_PATH = './guess_negative'
 
 for filename in glob.glob(os.path.join(POSITIVE_IMAGES_PATH, '*.jpg')):
     image = plt.imread(filename)
@@ -45,6 +48,11 @@ for filename in glob.glob(os.path.join(POSITIVE_GUESS_IMAGES_PATH, '*.jpg')):
     image_resized = misc.imresize(image, (image_size, image_size))
     dataset_guess.append(image_resized)
     y_guess.append(1)
+for filename in glob.glob(os.path.join(NEGATIVE_GUESS_IMAGES_PATH, '*.jpg')):
+    image = plt.imread(filename)
+    image_resized = misc.imresize(image, (image_size, image_size))
+    dataset_guess.append(image_resized)
+    y_guess.append(0)
 
 dataset = np.asarray(dataset)
 dataset_guess = np.asarray(dataset_guess)
@@ -52,15 +60,12 @@ y = np.asarray(y)
 y_guess = np.asarray(y_guess)
 
 dataset = dataset.reshape(dataset.shape[0], image_size*image_size*3)
-dataset_guess = dataset_guess(dataset_guess.shape[0], image_size*image_size*3)
+dataset_guess = dataset_guess.reshape(dataset_guess.shape[0], image_size*image_size*3)
 
 ####################################################################
 sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42, shuffle=True)
 
-#np.random.seed(42)
 X_train, X_test, y_train, y_test = train_test_split(dataset, y)
-X_train = np.asarray(X_train)
-y_train = np.asarray(y_train)
 #
 sgd_clf.fit(X_train, y_train)
 sgd_val = cross_val_predict(sgd_clf, X_test, y_test, cv=3)
@@ -69,7 +74,6 @@ print("sgd_acc: ", sgd_acc)
 ####################################################################
 sgd_clf_noisy = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42, shuffle=True)
 
-#np.random.seed(42)
 X_train_noisy = np.random.normal(X_train, 200)
 X_test_noisy = np.random.normal(X_test, 200)
 
@@ -78,8 +82,11 @@ sgd_val_noisy = cross_val_predict(sgd_clf_noisy, X_test_noisy, y_test, cv=3)
 sgd_acc_noisy = accuracy_score(sgd_val_noisy, y_test)
 print("sgd_acc_noisy: ", sgd_acc_noisy)
 ####################################################################
-guess = cross_val_predict(sgd_clf_noisy, dataset_guess, y_guess, cv=3)
-print(guess)
+np.random.seed(40)
+guess_val = cross_val_predict(sgd_clf, dataset_guess, y_guess, cv=3)
+guess_acc = accuracy_score(guess_val, y_guess)
+print(guess_val)
+print(guess_acc)
 ####################################################################
 #sgd_clf_pca = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
 #
