e443762c0c34c96c92842381d527062cc07165a6
Søren Mathiasen <zuperzam@email.dk>
2019-05-23 20:25:12 +0200

    Push

--- Eksamensprojekt/training_set.py
+++ Eksamensprojekt/training_set.py
@@ -17,33 +17,47 @@ import os
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
 dataset = []
+dataset_guess = []
 y = []
+y_guess = []
+image_size = 250
 
 POSITIVE_IMAGES_PATH = './shrek'
 NEGATIVE_IMAGES_PATH = './not_shrek_v2'
+POSITIVE_GUESS_IMAGES_PATH = './guess_positive'
+
 for filename in glob.glob(os.path.join(POSITIVE_IMAGES_PATH, '*.jpg')):
-    image = cv2.imread(filename)
-    image_resized = misc.imresize(image, (500, 500))
-#    print(image.shape)
-    print(image_resized.shape)
+    image = plt.imread(filename)
+    image_resized = misc.imresize(image, (image_size, image_size))
     dataset.append(image_resized)
     y.append(1)
-#print(y.shape)
 for filename in glob.glob(os.path.join(NEGATIVE_IMAGES_PATH, '*.jpg')):
-    image = cv2.imread(filename)
-    image_resized = misc.imresize(image, (500, 500))
+    image = plt.imread(filename)
+    image_resized = misc.imresize(image, (image_size, image_size))
     dataset.append(image_resized)
-    y.append('0')
+    y.append(0)
+    
+for filename in glob.glob(os.path.join(POSITIVE_GUESS_IMAGES_PATH, '*.jpg')):
+    image = plt.imread(filename)
+    image_resized = misc.imresize(image, (image_size, image_size))
+    dataset_guess.append(image_resized)
+    y_guess.append(1)
+
 dataset = np.asarray(dataset)
+dataset_guess = np.asarray(dataset_guess)
 y = np.asarray(y)
-print(dataset.shape)
+y_guess = np.asarray(y_guess)
 
-dataset = dataset.reshape(dataset.shape[0], 500*500*3)
+dataset = dataset.reshape(dataset.shape[0], image_size*image_size*3)
+dataset_guess = dataset_guess(dataset_guess.shape[0], image_size*image_size*3)
 
-sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
-#
+####################################################################
+sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42, shuffle=True)
+
+#np.random.seed(42)
 X_train, X_test, y_train, y_test = train_test_split(dataset, y)
 X_train = np.asarray(X_train)
 y_train = np.asarray(y_train)
@@ -51,7 +65,35 @@ y_train = np.asarray(y_train)
 sgd_clf.fit(X_train, y_train)
 sgd_val = cross_val_predict(sgd_clf, X_test, y_test, cv=3)
 sgd_acc = accuracy_score(sgd_val, y_test)
-print(sgd_acc)
+print("sgd_acc: ", sgd_acc)
+####################################################################
+sgd_clf_noisy = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42, shuffle=True)
+
+#np.random.seed(42)
+X_train_noisy = np.random.normal(X_train, 200)
+X_test_noisy = np.random.normal(X_test, 200)
+
+sgd_clf_noisy.fit(X_train_noisy, y_train)
+sgd_val_noisy = cross_val_predict(sgd_clf_noisy, X_test_noisy, y_test, cv=3)
+sgd_acc_noisy = accuracy_score(sgd_val_noisy, y_test)
+print("sgd_acc_noisy: ", sgd_acc_noisy)
+####################################################################
+guess = cross_val_predict(sgd_clf_noisy, dataset_guess, y_guess, cv=3)
+print(guess)
+####################################################################
+#sgd_clf_pca = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
+#
+#pca = PCA(n_components=0.2)
+#X_train_reduced = pca.fit_transform(X_train_noisy)
+#X_test_reduced  = pca.fit_transform(X_test_noisy)
+##X_recovered = pca.inverse_transform(X_reduced)
+#
+#sgd_clf_pca.fit(X_train_reduced, y_train)
+#sgd_val_pca = cross_val_predict(sgd_clf_pca, X_test_reduced, y_test, cv=3)
+#sgd_acc_pca = accuracy_score(sgd_val_pca, y_test)
+#print("sgd_acc_pca: ", sgd_acc_pca)
+####################################################################
+
 
 
 #logisticRegr = LogisticRegression(solver = 'lbfgs',max_iter = 1000, multi_class = 'multinomial')
